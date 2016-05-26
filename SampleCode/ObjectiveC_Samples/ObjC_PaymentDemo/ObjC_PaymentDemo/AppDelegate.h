3d6cf9b94ea739679dfcfb4e81bbf4747f53e72d
Efe Ariaroo <efeariaroo@gmail.com>
2016-05-26 19:40:31 +0100

    Adds objective c sample demo app for pay with sdk ui features

---
+++ SampleCode/ObjectiveC_Samples/ObjC_PaymentDemo/ObjC_PaymentDemo/AppDelegate.h
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.h
+//  ObjC_PaymentDemo
+//
+//  Created by Efe Ariaroo on 5/26/16.
+//  Copyright © 2016 Efe Ariaroo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) UIViewController *viewController;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ SampleCode/ObjectiveC_Samples/ObjC_PaymentDemo/SwiftSupport.swift
@@ -0,0 +1,9 @@
+//
+//  SwiftSupport.swift
+//  ObjC_PaymentDemo
+//
+//  Created by Efe Ariaroo on 5/26/16.
+//  Copyright © 2016 Efe Ariaroo. All rights reserved.
+//
+
+import Foundation
